b016c2e1a72b6b977565f91d1610c552b6c875f8
kislovairinar-beep <kislova.irina.r@gmail.com>
2025-10-18 16:24:29 +0500

    Update FileTest.cs

--- UnitTestEx/UnitTestEx/UnitTestProject/FileTest.cs
+++ UnitTestEx/UnitTestEx/UnitTestProject/FileTest.cs
@@ -1,44 +1,54 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using System;
-using UnitTestEx;
-using Assert = NUnit.Framework.Assert;
 
-namespace UnitTestProject
+namespace UnitTestingExamples.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class FileTest
     {
+        private File file;
 
-        public const string SIZE_EXCEPTION = "Wrong size";
-        public const string NAME_EXCEPTION = "Wrong name";
-        public const string SPACE_STRING = " ";
-        public const string FILE_PATH_STRING = "@D:\\JDK-intellij-downloader-info.txt";
-        public const string CONTENT_STRING = "Some text";
-        public double lenght;
-
-        /* ПРОВАЙДЕР */
-        static object[] FilesData =
+        [SetUp]
+        public void SetUp()
         {
-            new object[] {new File(FILE_PATH_STRING, CONTENT_STRING), FILE_PATH_STRING, CONTENT_STRING},
-            new object[] { new File(SPACE_STRING, SPACE_STRING), SPACE_STRING, SPACE_STRING}
-        };
-
-        /* Тестируем получение размера */
-        [Test, TestCaseSource(nameof(FilesData))]
-        public void GetSizeTest(File newFile, String name, String content)
-        {
-            lenght = content.Length / 2;
-
-            Assert.AreEqual(newFile.GetSize(), lenght, SIZE_EXCEPTION);
+            file = new File("test.txt", 100, "Test content");
         }
 
-        /* Тестируем получение имени */
-        [Test, TestCaseSource(nameof(FilesData))]
-        public void GetFilenameTest(File newFile, String name, String content)
+        [Test]
+        public void TestFileCreation()
         {
-            Assert.AreEqual(newFile.GetFilename(), name, NAME_EXCEPTION);
+            Assert.AreEqual("test.txt", file.GetFilename());
+            Assert.AreEqual(100, file.GetSize());
+            Assert.AreEqual("Test content", file.GetContent());
         }
 
+        [Test]
+        public void TestFileCreationWithEmptyName()
+        {
+            Assert.Throws<ArgumentException>(() => new File("", 100, "content"));
+        }
+
+        [Test]
+        public void TestFileCreationWithNegativeSize()
+        {
+            Assert.Throws<ArgumentException>(() => new File("test.txt", -1, "content"));
+        }
+
+        [Test]
+        public void TestFileCreationWithNullContent()
+        {
+            Assert.Throws<ArgumentNullException>(() => new File("test.txt", 100, null));
+        }
+
+        [Test]
+        public void TestFileEquals()
+        {
+            var file1 = new File("test.txt", 100, "content");
+            var file2 = new File("test.txt", 100, "content");
+            var file3 = new File("different.txt", 100, "content");
+
+            Assert.AreEqual(file1, file2);
+            Assert.AreNotEqual(file1, file3);
+        }
     }
 }
